f9534b349731f3d5d2e3f7715b62d8ddb353d800
nextset <nextset2005@yandex.ru>
2020-10-05 22:30:50 +0300

    init commit

--- next.py
+++ next.py
@@ -2,8 +2,18 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def pw():
+    import random
+    pas1 = list('1234567890')
+    pas2 = list('ABCDEFGHIGKLMNOPQRSTUVYXWZ')
+    pas3 = list('abcdefghigklmnopqrstuvyxwz')
+    random.shuffle(pas1)
+    random.shuffle(pas2)
+    random.shuffle(pas3)
+    passwd = ''.join([random.choice(pas1) for x in range(4)] + [random.choice(pas2) for x in range(1)] + [random.choice(pas3) for x
+                                                                                             in range(1)])
+    return passwd
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
